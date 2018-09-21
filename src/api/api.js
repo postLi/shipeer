5b80c428bf5e36b6dec485b163b88e967db67a4e
huangjx@anfawuliu.com <huangjx@anfawuliu.com>
2018-09-21 09:06:18 +0800

    update api

--- src/api/api.js
+++ src/api/api.js
@@ -3,6 +3,6 @@ import axios from 'axios'
 let timeOut = {timeout: 1000 * 60};
 
 // 通用接口
-export const postApi = (url,params,conf) => { return axios.post(url, params,conf).then(res => res.data); };
+export const postApi = (url,params) => { return axios.post(url, params,timeOut).then(res => res.data); };
 
-export const getApi = url => { return axios.get(url).then(res => res.data); };
+export const getApi = url => { return axios.get(url,timeOut).then(res => res.data); };
