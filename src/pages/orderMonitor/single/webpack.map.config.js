5d8ae770d08247840755c7a79defd3db102642e8
Peng <dengp@anfawuliu.com>
2018-10-07 21:10:25 +0800

    在途监控

--- src/pages/orderMonitor/single/webpack.map.config.js
+++ src/pages/orderMonitor/single/webpack.map.config.js
@@ -2,45 +2,48 @@ const webpack = require("webpack");
 const path = require('path');
 
 module.exports = {
-    entry: {
-        map: __dirname + "/app/map.js"
-    },
-    output: {
-        path: __dirname + "/dist/webapp/js",
-        filename: "[name].js",
-        publicPath: ""
-    },
-    module: {
-        rules: [{
-            test: /(\.jsx|\.js)$/,
-            use: {
-                loader: "babel-loader"
-            },
-            exclude: /node_modules/
+  entry: {
+    map: __dirname + "./orderMonitor.js"
+  },
+  output: {
+    path: __dirname + "../dist/js",
+    filename: "[name].js",
+    publicPath: ""
+  },
+  module: {
+    rules: [{
+      test: /(\.jsx|\.js)$/,
+      use: {
+        loader: "babel-loader"
+      },
+      exclude: /node_modules/
+    }, {
+      test: /\.css$/,
+      use: [
+        {
+          loader: "style-loader"
         }, {
-            test: /\.css$/,
-            use: [
-                {
-                    loader: "style-loader"
-                }, {
-                    loader: "css-loader"
-                }
-            ]
-        }, {
-            test: /\.(jpg|png|gif|svg)$/,
-            loader: 'url-loader?limit=8192&name=img/[name].[ext]?[hash]'
-        }]
-    },
-    watch: false,
-    devServer: {
-        host: '0.0.0.0',
-        port: 8081,
-        contentBase: path.resolve(__dirname, './static'),
-        publicPath: "/js/",
-        inline: true,
-        hot: true
-    },
-    plugins: [
-        new webpack.HotModuleReplacementPlugin()
-    ]
+          loader: "css-loader"
+        }
+      ]
+    }, {
+      test: /\.(jpg|png|gif|svg)$/,
+      loader: 'url-loader?limit=8192&name=img/[name].[ext]?[hash]'
+    }, {
+      test: /\.vue$/,
+      loader: 'vue-loader'
+    }]
+  },
+  watch: false,
+  devServer: {
+    host: '0.0.0.0',
+    port: 8081,
+    contentBase: path.resolve(__dirname, '../dist'),
+    publicPath: "/js/",
+    inline: true,
+    hot: true
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ]
 };
