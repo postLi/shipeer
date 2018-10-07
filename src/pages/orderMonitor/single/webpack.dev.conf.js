5eafd397c2ef87681516a3fea5a5885a31e1379d
Peng <dengp@anfawuliu.com>
2018-10-07 10:19:00 +0800

    在途监控

--- src/pages/orderMonitor/single/webpack.dev.conf.js
+++ src/pages/orderMonitor/single/webpack.dev.conf.js
@@ -26,8 +26,7 @@ module.exports = {
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
         loader: 'url-loader',
         options: {
-          limit: 10000,
-          name: utils.assetsPath('img/[name].[hash:7].[ext]')
+          limit: 10000
         }
       }
     ]
